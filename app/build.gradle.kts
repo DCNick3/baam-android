bbfe30a2f55432a4a7820af3532d986b0912d95b
⭐️NINIKA⭐️ <dcnick3@users.noreply.github.com>
2025-09-12 00:19:19 +0300

    Update version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "me.dcnick3.baam"
         minSdk = 21
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
