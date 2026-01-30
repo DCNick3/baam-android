eab591382bd9d6e7bbed626f0c7258b5701d760e
⭐️NINIKA⭐️ <dcnick3@users.noreply.github.com>
2026-01-30 14:41:04 +0300

    Update version to 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "me.dcnick3.baam"
         minSdk = 21
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
